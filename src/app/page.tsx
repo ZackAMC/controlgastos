2d6eeab524c87f454d1bc8ceaa9d4c928d95e158
ZackAMC <andresmila01@gmail.com>
2025-06-20 06:29:58 -0500

    Ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import styles from "./page.module.css";
 
 export default function Home() {
   const [tipoGasto, setTipoGasto] = useState("");
